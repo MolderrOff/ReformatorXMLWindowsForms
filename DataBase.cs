da7fe38cb7d8b3cfa9af661515c6da29fa8abe1b
MolderrOff <pashkent@list.ru>
2025-08-20 22:13:09 +0500

    Добавьте файлы проекта.

---
+++ DataBase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using for_video;
+using System.Windows.Forms;
+using System.Data;
+
+
+
+namespace for_video
+{
+    //было "Data Source=MOLDERR-PC\SQLEXPRESS;Initial Catalog=test;Integrated Security=True"
+    internal class DataBase
+    {
+        
+        SqlConnection sqlConnection = new SqlConnection(@"Server=localhost\SQLEXPRESS;Database=testdb1;Trusted_Connection=True;");
+
+        public bool CheckDB()
+        {
+            //SqlConnection myConnection = new SqlConnection(sqlConnection);
+            try
+            {
+                openConnection();
+                //sqlConnection.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task createMyDataBase()
+        {
+            //DataBase database = new DataBase();
+            ////SqlConnection sqlConnection = new SqlConnection();
+            ////sqlConnection.ConnectionString = @"Server=localhost\SQLEXPRESS;Database=testdb1;Trusted_Connection=True;";
+            using (SqlConnection sqlConnection = new SqlConnection(@"Server=localhost\SQLEXPRESS;Trusted_Connection=True;")) //@"Server=localhost\SQLEXPRESS;Trusted_Connection=True;"
+            {
+                await sqlConnection.OpenAsync();
+                SqlCommand myCommand = new SqlCommand("CREATE DATABASE testdb1", sqlConnection);
+                await myCommand.ExecuteNonQueryAsync();
+                MessageBox.Show("База данных СОЗДАНА");
+            };
+            MessageBox.Show("Между созданием БД И Таблица");
+            using (SqlConnection sqlConnection2 = new SqlConnection(@"Server=localhost\SQLEXPRESS;Database=testdb1;Trusted_Connection=True;"))
+            {
+                MessageBox.Show("Перед созданием Таблица ");
+                string str = "CREATE TABLE Register (Id_user INTEGER NULL, Login_user nvarchar(2048) NULL, Password_user NVARCHAR(2048) NULL);";
+                await sqlConnection2.OpenAsync();
+                SqlCommand myCommand2 = new SqlCommand(str, sqlConnection2);
+                await myCommand2.ExecuteNonQueryAsync();
+                MessageBox.Show("Таблица СОЗДАНА");
+                Console.WriteLine("Таблица Users создана");
+            };
+                             
+                       
+            //sqlConnection.Close();
+            
+            //DataTable register = new DataTable();
+
+            //SqlDataAdapter adapter2 = new SqlDataAdapter(myCommand2);
+            //adapter2.Fill(register);
+            //sqlConnection2.Close();
+
+        }
+        public void openConnection()
+        {
+
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
---
+++ log_in.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using for_video;
+using ReformatorXMLWindowsForms;
+
+
+namespace for_video
+{
+    public partial class log_in : Form
+    {
+        DataBase database = new DataBase();
+        public log_in()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+            Console.WriteLine("из файла log_in");
+        }
+
+        private void log_in_Load(object sender, EventArgs e)
+        {
+            textBox_password.PasswordChar = '*';
+            pictureBox6.Visible = false;
+            textBox_login.MaxLength= 50;
+            textBox_password.MaxLength = 50;
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            var loginUser = textBox_login.Text;
+            var passUser = textBox_password.Text;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string querystring = $"select id_user, login_user, password_user from register where login_user = '{loginUser}' and password_user = '{passUser}'";
+
+            Console.WriteLine("Из log_in.cs btnEnter_Click");
+
+
+            if (database.CheckDB() == true)
+            {
+                SqlCommand command = new SqlCommand(querystring, database.getConnection());
+
+
+
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+
+                if (table.Rows.Count == 1)
+                {
+                    MessageBox.Show("Вы успешно вошли!", "Успешно!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    //MessageBox.Show("Из log_in.cs btnEnter_Click сообщение 2");
+                    Form1 frm1 = new Form1(); //было
+                    //Form2 frm1 = new Form2(); //cтало
+                    //Form3 frm1 = new Form3(); //cтало
+                    this.Hide();
+                    frm1.ShowDialog();
+                    this.Show();
+                }
+                else
+                    MessageBox.Show("Такого аккаунта не существует!", "Аккаунта не существует!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            { MessageBox.Show("База данных не существует. Создайте БД"); }
+
+        }
+
+        private void linkLabel1_Click(object sender, EventArgs e)
+        {
+            sign_up frm_sign = new sign_up();
+            frm_sign.Show();
+            this.Hide();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox_login.Text = "";
+            textBox_password.Text = "";
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            textBox_password.UseSystemPasswordChar = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = true;
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            textBox_password.UseSystemPasswordChar = true;
+            pictureBox5.Visible = true;
+            pictureBox6.Visible = false;
+        }
+        
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            sign_up frm_sign = new sign_up();
+            frm_sign.Show();
+            this.Hide();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBox_login.Text = "";
+            textBox_password.Text = "";
+
+        }
+
+        private void log_in_Load_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox_login_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
